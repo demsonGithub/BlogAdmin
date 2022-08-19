5d1e5962f0cb405223fa773342dc7c723524692d
wangzhixiong <demkin@163.com>
2022-08-19 17:52:28 +0800

    角色详情

--- Demkin.Blog.DTO/Role/RoleDetailDto.cs
+++ Demkin.Blog.DTO/Role/RoleDetailDto.cs
@@ -1,11 +1,17 @@
-﻿namespace Demkin.Blog.DTO.Role
+﻿using Demkin.Blog.Enum;
+
+namespace Demkin.Blog.DTO.Role
 {
     public class RoleDetailDto
     {
+        public long Id { get; set; }
+
         public string RoleName { get; set; }
 
         public string Description { get; set; }
 
         public int SortNumber { get; set; }
+
+        public Status Status { get; set; }
     }
 }
